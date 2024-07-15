d4396fbfa9b4ad3c84e3dd2777413ec7663ae8f6
ajit_dandwatee <E1dd99y8@>
2024-07-15 11:59:14 +0530

    New first commint

---
+++ src/test/java/app/TestApplication.java
@@ -0,0 +1,103 @@
+package app;
+
+import static org.mockito.Mockito.when;
+
+import app.exception.RequestException;
+import app.payload.ProductPayload;
+import app.service.HttpClient;
+import app.service.HttpClientImpl;
+import app.service.ProductService;
+import app.service.ProductServiceImpl;
+import java.io.IOException;
+import java.util.UUID;
+import okhttp3.Protocol;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestApplication {
+
+  @Mock
+  private HttpClient httpClient;
+
+  @Test(expected = RequestException.class)
+  public void testMockito() {
+    // Create the product payload for testing
+    ProductPayload productPayload = ProductPayload.builder()
+            .model("Iphone XR")
+            .price(5358.99)
+            .build();
+
+    // Mock behavior of HttpClient
+    when(httpClient.post("/api/v1/products", productPayload))
+            .thenThrow(new RequestException("Test"));
+
+    // Create ProductService with mocked HttpClient
+    ProductService productService = new ProductServiceImpl(httpClient);
+
+    // Call the method under test, expecting it to throw RequestException
+    productService.createProduct(productPayload);
+  }
+
+    @Test(expected = RequestException.class)
+    public void testWithoutLibrary() {
+
+      HttpClientMock httpClient = new HttpClientMock("http://google.com");
+      ProductService productService = new ProductServiceImpl(httpClient);
+
+      // Mock HttpClientMock
+      Request request = new Request.Builder()
+        .url("http://test.com")
+        .build();
+
+      Response response = new Response.Builder()
+        .code(500)
+        .request(request)
+        .protocol(Protocol.HTTP_1_1)
+        .message(UUID.randomUUID().toString())
+        .build();
+
+      httpClient.setMockResponse(response);
+
+      // Test request
+      ProductPayload productPayload = ProductPayload.builder()
+        .model("Iphone XR")
+        .price(5358.99)
+        .build();
+
+      productService.createProduct(productPayload);
+    }
+
+    @Test(expected = RequestException.class)
+    public void testMockWebServer() throws IOException {
+      // Start MockWebServer
+      MockWebServer mockWebServer = new MockWebServer();
+      mockWebServer.start();
+
+      // Mock response
+      MockResponse mockResponse = new MockResponse();
+      mockResponse.setResponseCode(500);
+      mockResponse.setBody("{}");
+
+      // Enqueue response
+      mockWebServer.enqueue(mockResponse);
+
+      // Test request
+      String url = String.format("http://%s:%s", mockWebServer.getHostName(), mockWebServer.getPort());
+      HttpClient httpClient = new HttpClientImpl(url);
+      ProductService productService = new ProductServiceImpl(httpClient);
+
+      ProductPayload productPayload = ProductPayload.builder()
+        .model("Iphone XR")
+        .price(5358.99)
+        .build();
+
+      productService.createProduct(productPayload);
+    }
+}
---
+++ src/test/java/app/service/ProductServiceImpl.java
@@ -0,0 +1,47 @@
+package app.service;
+
+import app.domain.Product;
+import app.exception.RequestException;
+import app.payload.ProductPayload;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.logging.Level;
+import lombok.NonNull;
+import lombok.extern.java.Log;
+import okhttp3.Response;
+
+@Log
+public class ProductServiceImpl implements ProductService {
+
+  private final ObjectMapper objectMapper = new ObjectMapper();
+  private final HttpClient httpClient;
+
+  public ProductServiceImpl(HttpClient httpClient) {
+    this.httpClient = httpClient;
+  }
+
+  public Product createProduct(@NonNull ProductPayload payload) {
+    Product product;
+
+    Response response = httpClient.post("/api/v1/products", payload);
+    if (response.isSuccessful()) {
+      String body = response.body().toString();
+      product = convertJsonToProduct(body);
+    } else {
+      throw new RequestException(
+        String.format("Error to create Product=[%s] Status=[%s]", payload, response.code()));
+    }
+
+    log.log(Level.INFO, String.format("Created Product=[%s]", product));
+    return product;
+  }
+
+  private Product convertJsonToProduct(@NonNull String json) {
+    try {
+      return objectMapper.readValue(json, Product.class);
+    } catch (IOException e) {
+      throw new RequestException(String.format("Error to create Product, invalid json=[%s]", json), e);
+    }
+  }
+
+}
